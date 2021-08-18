797dd8813c62518850799270fdc0ce30aef527e7
Roman Bulakh <bulah.roman@gmail.com>
2021-08-18 11:07:11 +0300

    starting scripts and env

---
+++ getApi.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+#
+# getApi.sh
+# Copyright (C) 2021 Roman Bulakh <bulah.roman@gmail.com>
+#
+# Distributed under terms of the MIT license.
+#
+
+wget https://raw.githubusercontent.com/osrg/gobgp/master/api/attribute.proto -O api/attribute.proto
+wget https://raw.githubusercontent.com/osrg/gobgp/master/api/capability.proto -O api/capability.proto
+wget https://raw.githubusercontent.com/osrg/gobgp/master/api/gobgp.proto -O api/gobgp.proto
+
+python -m grpc_tools.protoc -I./api/ --python_out=./api/ --grpc_python_out=./api/ api/*.proto 
